5b9c140456b8b971f31c0f80cd0cef3da2cc29ef
faii <nurrahmanrifai3@gmail.com>
2025-05-16 00:00:49 +0700

    update contact

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -2,7 +2,7 @@
 <template>
     <a
       :href="url"
-      class="w-full sm:w-auto rounded-full bg-gray-400 px-5 sm:px-6 py-2 sm:py-3 font-semibold shadow-lg transition-all hover:bg-gray-500 hover:scale-[1.1] text-sm sm:text-base text-center"
+      class="w-full sm:w-auto rounded-lg bg-gray-400 px-5 sm:px-6 py-2 sm:py-3 font-semibold shadow-lg transition-all hover:bg-gray-500 hover:scale-[1.1] text-sm sm:text-base text-center"
     >
       <slot/>
     </a>
